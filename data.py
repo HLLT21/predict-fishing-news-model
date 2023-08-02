217591794cf8983bc953e9e3518ecaea4ac3a93d
신누리 <nr8122@gmail.com>
2023-08-02 17:35:06 +0900

    data.py 수정위치 잘 확인하셔서 수정해주시면 json->csv로 뽑으실 수 있을거에요 ~ 수고하세요~

--- data.py
+++ data.py
@@ -4,10 +4,17 @@ import pandas as pd
 
 
 
-columns = ['newsTitle', 'newsContent', 'clickbaitClass']
+'''
+[제목, 내용, 카테고리, 낚시성]
+'''
+columns = ['newsTitle', 'newsContent', 'newsCategory','clickbaitClass']
 sample = []
 count = 1
+
 def get_data(path):
+    '''
+    [제목, 내용, 카테고리, 낚시성] 순으로 데이터를 모음 알아서 해석하셈
+    '''
     global columns
     global sample
     global count 
@@ -16,29 +23,36 @@ def get_data(path):
         data = json.loads(f.read())
         newsTitle = data['sourceDataInfo']['newsTitle']
         newsContent = data['sourceDataInfo']['newsContent']
+        newsCategory = data['sourceDataInfo']['newsCategory']
         clickbaitClass = data['labeledDataInfo']['clickbaitClass']
-        sample.append([newsTitle, newsContent, clickbaitClass])
+        sample.append([newsTitle, newsContent, newsCategory, clickbaitClass])
         print(count)
         count+=1
         # pd.DataFrame(columns=columns, data=sample).to_csv('/Users/mac/Desktop/github/predict-fishing-news/data/data_frame.csv')
         
+
 def bruteforce(path):
+    '''
+    폴더안의 파일을 읽은 후 폴더면 다시 재귀, .json이 포함되어 있으면 데이터 획득하는 get_data함수 실행
+    '''
     for file in os.listdir(path):
         temp = path+'/'+file
         if os.path.isdir(temp):
             bruteforce(temp)
-        else:
+        elif '.json' in file:
             get_data(temp)
-# path = '/Users/mac/Downloads/146.낚시성 기사 탐지 데이터/01.데이터/Training/02.라벨링데이터'
-# os.chdir(path)
-# for file in os.listdir():
-#     if os.path.isdir(file):
-#         bruteforce(path+'/'+file)  
-    
-# pd.DataFrame(columns=columns, data=sample, index=None).to_csv('/Users/mac/Desktop/github/predict-fishing-news/data/data_frame.csv')
-    
-# print('end')
+# path : 라벨링데이터 폴더 위치
+# 예시
+# path = 'C:/Users/gjaischool1/Downloads/146.낚시성 기사 탐지 데이터/01.데이터/Training/02.라벨링데이터'
+path = '라벨링데이터 폴더 위치' ## 수정위치
+os.chdir(path)
+for file in os.listdir():
+    if os.path.isdir(file):
+        bruteforce(path+'/'+file)  
+# 데이터 프레임을 저장할 위치 
+# 예시
+# pd.DataFrame(columns=columns, data=sample, index=None).to_csv('C:/Users/gjaischool1/github/predict-fishing-news-model/data/dataset.csv')
+pd.DataFrame(columns=columns, data=sample, index=None).to_csv('데이터 프레임 저장할 위치') ## 수정위치
+print('end')
 
 
-df = pd.read_csv('data/data_frame.csv')
-print(df.shape)
